088750a51cc14c87c239de0b8f27771bd3497beb
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-02-14 13:55:52 +0300

    -5.3. Method syntax (Rectangle exp)

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-    let mut user1 = User{
-        username: String::from("ahmed1"),
-        email: "ahmed@gmail.com".to_string(),
-        sign_in_count: 1,
-        active: true,
-    };
-    user1.email = ("ahmed2").to_string();
-    let user2= User{
-        username: "moh".to_string(),
-        email: "moh@gmail.com".to_string(),
-        ..user1
-    };
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-fn build_user (email: String, username:String) -> User{
-    User{
-        username,
-        email,
-        sign_in_count: 1,
-        active: true,
+impl Rectangle{
+    fn area (&self) -> u32{
+        self.width * self.height
     }
+    fn circul (&self) -> u32{
+        self.width*2 + self.height*2
+    }
+}
+
+fn main() {
+    let rec1 =Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(" the area of the rectangle is: {} \n \
+                the lines lenth of regtnagle is: {}",
+                    rec1.area(), rec1.circul())
 
 }
